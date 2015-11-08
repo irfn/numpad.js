04022948dd13b9c998951a98d3dc4e5f8038d88d
Irfan Shah <mail.urfi@gmail.com>
2015-11-08 18:39:14 +0530

    using dom loaded

--- numpad.js
+++ numpad.js
@@ -1,53 +1,56 @@
-var shiftKeyPressed = false;
-var iframe = document.createElement('iframe');
-iframe.id = "numpadIframe";
-var iframecss = "#numpadIframe {margin:0;padding: 0;width:149px;height: 192px; position: absolute; bottom: 0;right:0; border: none}";
+document.addEventListener("DOMContentLoaded", function(event) {
+    var shiftKeyPressed = false;
+    var iframe = document.createElement('iframe');
+    iframe.id = "numpadIframe";
+    var iframecss = "#numpadIframe {margin:0;padding: 0;width:149px;height: 192px; position: absolute; bottom: 0;right:0; border: none}";
 
-var iframeStyle = document.createElement('style');
-iframeStyle.type = 'text/css';
-iframeStyle.appendChild(document.createTextNode(iframecss));
-document.body.appendChild(iframeStyle);
+    var iframeStyle = document.createElement('style');
+    iframeStyle.type = 'text/css';
+    iframeStyle.appendChild(document.createTextNode(iframecss));
+    document.body.appendChild(iframeStyle);
 
-iframe.onload = function() {
-    var doc = iframe.contentDocument || iframe.contentWindow.document;
-    var numpad = doc.createElement('div');
-    var style = doc.createElement('style');
-    style.type = 'text/css';
-    var css = "body{ width: 100%;margin:0;} .numpad { width: 139px; border: 1px solid #6D6D6D; background-color: #777777; padding: 4px; border-radius: 5px; } .key { font-size: 18px; width: 40px; height: 40px; display: inline-block; line-height: 40px; text-align: center; outline: none; margin: 3px; } .clicked { background: #AFAFAF; border: none;}"
-    style.appendChild(document.createTextNode(css));
-    numpad.className = "numpad";
-    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", '0', "#"].forEach(function(id) {
-        var button = doc.createElement('button');
-        button.className = "key";
-        button.id = id;
-        button.value = id;
-        button.appendChild(doc.createTextNode(id));
-        numpad.appendChild(button);
+    iframe.onload = function() {
+        var doc = iframe.contentDocument || iframe.contentWindow.document;
+        var numpad = doc.createElement('div');
+        var style = doc.createElement('style');
+        style.type = 'text/css';
+        var css = "body{ width: 100%;margin:0;} .numpad { width: 139px; border: 1px solid #6D6D6D; background-color: #777777; padding: 4px; border-radius: 5px; } .key { font-size: 18px; width: 40px; height: 40px; display: inline-block; line-height: 40px; text-align: center; outline: none; margin: 3px; } .clicked { background: #AFAFAF; border: none;}"
+        style.appendChild(document.createTextNode(css));
+        numpad.className = "numpad";
+        ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", '0', "#"].forEach(function(id) {
+            var button = doc.createElement('button');
+            button.className = "key";
+            button.id = id;
+            button.value = id;
+            button.appendChild(doc.createTextNode(id));
+            numpad.appendChild(button);
 
-    })
-    doc.body.appendChild(numpad);
-    doc.body.appendChild(style);
-    doc.getElementById('*').onclick = function() {
-        console.log("clicked me");
-    }
-
-    function keyDown(e) {
-        if (e.shiftKey) {
-            shiftKeyPressed = true;
-        } else {
-            shiftKeyPressed = false;
+        })
+        doc.body.appendChild(numpad);
+        doc.body.appendChild(style);
+        doc.getElementById('*').onclick = function() {
+            console.log("clicked me");
         }
-        var key = getKey(e.keyCode);
-        if (key !== undefined) {
-            var element = doc.getElementById(key);
-            clickEffect(element);
-            element.click();
+
+        function keyDown(e) {
+            if (e.shiftKey) {
+                shiftKeyPressed = true;
+            } else {
+                shiftKeyPressed = false;
+            }
+            var key = getKey(e.keyCode);
+            if (key !== undefined) {
+                var element = doc.getElementById(key);
+                clickEffect(element);
+                element.click();
+            }
         }
-    }
-    document.addEventListener("keydown", keyDown, false);
-    document.addEventListener("keyup", keyUp, false);
-};
-document.body.appendChild(iframe);
+        document.addEventListener("keydown", keyDown, false);
+        document.addEventListener("keyup", keyUp, false);
+    };
+    document.body.appendChild(iframe);
+});
+
 
 
 function keyUp(e) {
